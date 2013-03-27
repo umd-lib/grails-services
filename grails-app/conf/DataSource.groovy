33865c31d5814d84f96bafd0fb9ff4f7d49f7d10
Aaron Scheffel <ascheffe@umd.edu>
2013-03-27 08:55:37 -0400

    Updated the datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -31,18 +31,8 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            pooled = false
+            jndiName = "java:comp/env/jdbc/gsDS"
         }
     }
 }
